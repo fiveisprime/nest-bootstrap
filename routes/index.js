2d45808bc4090f224d9387ab038821189f240cc4
Matt Hernandez <matt@modulus.io>
2013-03-20 08:22:12 -0400

    More error handling rework and updated example.

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ var config = require('../config')
   , nest   = require('unofficial-nest-api');
 
 exports.index = function(req, res) {
-  nest.login(config.username, config.password, function(data) {
-    if (!data) {
-      res.render('error', { error: 'Login failed' });
+  nest.login(config.username, config.password, function(err, data) {
+    if (err) {
+      res.render('error', { error: err.message });
+    } else {
+      nest.get(function(info) {
+        res.render('index', { nest: info });
+      });
     }
-    nest.get(function(info) {
-      res.render('index', { nest: info });
-    });
   });
 };
